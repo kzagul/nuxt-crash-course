bf3538ea0e6031d2af1eb53733b161aa1a4068f2
Kirill Zagul <zagulkirill@gmail.com>
2022-08-29 11:00:03 +0500

    stack page added

--- pages/stack.vue
+++ pages/stack.vue
@@ -24,6 +24,71 @@
                     <v-icon>mdi-language-html5</v-icon>
                 </v-btn>
             </v-col>
+            <v-col cols="12" sm="3">
+                <v-btn icon color="indigo">
+                    <v-icon>mdi-git</v-icon>
+                </v-btn>
+            </v-col>
+            <v-col cols="12" sm="3">
+                <v-btn icon color="indigo">
+                    <v-icon>mdi-language-css3</v-icon>
+                </v-btn>
+            </v-col>
+            <v-col cols="12" sm="3">
+                <v-btn icon color="indigo">
+                    <v-icon>mdi-language-csharp</v-icon>
+                </v-btn>
+            </v-col>
+            <v-col cols="12" sm="3">
+                <v-btn icon color="indigo">
+                    <v-icon>mdi-language-php</v-icon>
+                </v-btn>
+            </v-col>
+            <v-col cols="12" sm="3">
+                <v-btn icon color="indigo">
+                    <v-icon>mdi-material-ui</v-icon>
+                </v-btn>
+            </v-col>
+            <v-col cols="12" sm="3">
+                <v-btn icon color="indigo">
+                    <v-icon>mdi-nodejs</v-icon>
+                </v-btn>
+            </v-col>
+            <v-col cols="12" sm="3">
+                <v-btn icon color="indigo">
+                    <v-icon>mdi-npm</v-icon>
+                </v-btn>
+            </v-col>
+            <v-col cols="12" sm="3">
+                <v-btn icon color="indigo">
+                    <v-icon>mdi-nuxt</v-icon>
+                </v-btn>
+            </v-col>
+            <v-col cols="12" sm="3">
+                <v-btn icon color="indigo">
+                    <v-icon>mdi-sass</v-icon>
+                </v-btn>
+            </v-col>
+             <v-col cols="12" sm="3">
+                <v-btn icon color="indigo">
+                    <v-icon>mdi-tailwind</v-icon>
+                </v-btn>
+            </v-col>
+             <v-col cols="12" sm="3">
+                <v-btn icon color="indigo">
+                    <v-icon>mdi-webpack</v-icon>
+                </v-btn>
+            </v-col>
+             <v-col cols="12" sm="3">
+                <v-btn icon color="indigo">
+                    <v-icon>mdi-vuejs</v-icon>
+                </v-btn>
+            </v-col>
+             <v-col cols="12" sm="3">
+                <v-btn icon color="indigo">
+                    <v-icon>mdi-vuetify</v-icon>
+                </v-btn>
+            </v-col>
           
         </v-row>
 
